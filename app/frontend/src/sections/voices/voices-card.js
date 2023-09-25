ae7c9aaea25095ac8745d0e3cce8375f68c8aa5f
JOETtheIV <venti1217@qq.com>
2023-09-25 19:26:21 +0800

    add voice select preview

--- app/frontend/src/sections/voices/voices-card.js
+++ app/frontend/src/sections/voices/voices-card.js
@@ -1,61 +1,73 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useImperativeHandle } from 'react';
 import { Card, Typography, Avatar, Box, IconButton } from '@mui/material';
 import PlayIcon from '@mui/icons-material/PlayCircleOutline';
 import PauseIcon from '@mui/icons-material/PauseCircleOutline';
 import { WordPill } from 'src/components/word-pill';
+import { voices, statusMap, voiceAvatarMap, voiceAudioMap } from './constants';
 
-import { voices, statusMap, voiceAvatarMap } from './constants';
+const VoicesCard = React.forwardRef(({ index, attributes, onClick, playingAudioRef }, ref) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audio, setAudio] = useState(null);
+  const voicePath = voiceAudioMap[voices[index - 1]];
 
-export const VoicesCard = ({ index, attributes, onClick }) => {
-  const [isPlaying, setIsPlaying] = useState(false);  // State to manage play/pause
+  useImperativeHandle(ref, () => ({
+    stopAudio: () => {
+      if (audio) {
+        audio.pause();
+        setIsPlaying(false);
+        setAudio(null);
+      }
+    }
+  }));
 
-  // Function to handle play/pause button click
   const handlePlayPauseClick = (event) => {
-    event.stopPropagation();  // Prevent card click event from firing
-    setIsPlaying(!isPlaying);  // Toggle state
-    //  play/pause logic here
+    console.log("Voice path being played:", voicePath);
+    event.stopPropagation();
+
+    if (audio) {
+      audio.pause();
+      setIsPlaying(false);
+      setAudio(null);
+      if (playingAudioRef.current === audio) {
+        playingAudioRef.current = null;
+      }
+    } else {
+      if (playingAudioRef.current) {
+        playingAudioRef.current.pause();
+      }
+      const newAudio = new Audio(voicePath);
+      setAudio(newAudio);
+      newAudio.play();
+      setIsPlaying(true);
+      playingAudioRef.current = newAudio;
+    }
   };
 
+  useEffect(() => {
+    return () => {
+      if (audio) {
+        audio.pause();
+        setAudio(null);
+      }
+    };
+  }, [audio]);
+
   return (
-    <Card sx={{ p: 1, 
-      display: 'flex', 
-      justifyContent: 'left', 
-      alignItems: 'center', 
-      cursor: 'pointer',  // Change cursor to pointer on hover
-      '&:hover': {  // Hover effect
-        backgroundColor: 'primary.lightest',
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
-    }}}
-      onClick={onClick? onClick : null}  // If onClick prop is passed, add click event listener
-    >
-      <IconButton color= {isPlaying ? 'success' : 'primary'}
-          onClick={handlePlayPauseClick} 
-          sx={{
-            height: 45,
-            width: 45, 
-            fontSize: '2.5rem'
-          }}
-      >
+    <Card sx={{ p: 1, display: 'flex', justifyContent: 'left', alignItems: 'center', cursor: 'pointer', '&:hover': { backgroundColor: 'primary.lightest', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' } }} onClick={onClick ? onClick : null}>
+      <IconButton color={isPlaying ? 'success' : 'primary'} onClick={handlePlayPauseClick} sx={{ height: 45, width: 45, fontSize: '2.5rem' }}>
         {isPlaying ? <PauseIcon fontSize="inherit" /> : <PlayIcon fontSize="inherit" />}
       </IconButton>
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Avatar
-            sx={{
-              height: 45,
-              width: 45,
-              bgcolor: isPlaying ? 'success.main' : 'primary.main',
-            }}
-          src={voiceAvatarMap[voices[index-1]]}
-        />
+        <Avatar sx={{ height: 45, width: 45, bgcolor: isPlaying ? 'success.main' : 'primary.main' }} src={voiceAvatarMap[voices[index - 1]]} />
         <Box sx={{ marginLeft: 2 }}>
-          <Typography variant="h6">{voices[index-1]}</Typography>
-          <Box sx={{ display: 'flex', gap: 1, pt:1 }}>
-            {Object.keys(attributes).map((key, i) => (
-              <WordPill key={i} color={statusMap[key]}>{attributes[key]}</WordPill>
-            ))}
+          <Typography variant="h6">{voices[index - 1]}</Typography>
+          <Box sx={{ display: 'flex', gap: 1, pt: 1 }}>
+            {Object.keys(attributes).map((key, i) => (<WordPill key={i} color={statusMap[key]}>{attributes[key]}</WordPill>))}
           </Box>
         </Box>
       </Box>
     </Card>
   );
-};
+});
+
+export { VoicesCard };
